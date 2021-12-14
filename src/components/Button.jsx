52d472e34681208fd8d9e97619279bc1c2804822
Marcos <nodmarcos@gmail.com>
2021-12-14 17:54:22 -0300

    layout changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,18 @@
 import './Button.css';
 
 export default props => {
+    let classes = 'button ';
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+
+
     return (
-        <button className="button">{props.label}</button>
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}
+        >
+            {props.label}
+        </button>
     )
 }
